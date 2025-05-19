195b52023891b38776f5a3315890a3d24f889a2d
chaerultrisusilo <trisusilochaerul@gmail.com>
2025-05-19 11:06:22 +0700

    first Commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form | Portofolio Hamzah</title>
+    <link
+        rel="stylesheet"
+        href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="login-form mt-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">
+                                Login Form
+                            </div>
+                            <div class="card-body">
+                                <form action="" method="post">
+                                    <div class="mb-3">
+                                        <label for="form-label">Email</label>
+                                        <input type="email" class="form-control" id="email" name="email"
+                                            placeholder="Ex: admin@gmail.com">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="form-label">Password</label>
+                                        <input type="password" class="form-control" id="email" name="email"
+                                            placeholder="Masukan Password Anda">
+                                    </div>
+                                    <div class="mb-3">
+                                        <button type="submit" class="btn btn-primary"></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+</body>
+
+</html>
